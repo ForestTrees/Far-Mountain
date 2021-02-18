d724d4fcef7e1d2da32ec6fb83c05162eca8887e
BuildTools <unconfigured@null.spigotmc.org>
2021-02-18 17:47:07 +0800

    js混淆加密

--- js/alert.js
+++ js/alert.js
@@ -1,51 +1,10 @@
-function limit() {
-	var div = $(".max_limit");
-	if (div.is(":animated")) { //判断元素是否正处于动画状态
-	} else {
-		//这里写新的动画
-		div.animate({
-			top: '10px',
-			opacity: 1
-		}, 300);
-		div.animate({
-			top: '10px',
-			opacity: 1
-		}, 2000);
-		div.animate({
-			top: '-50px',
-			opacity: 0
-		}, 300);
-	}
-}
+function limit() { var T1 = $("\x2e\x6d\x61\x78\x5f\x6c\x69\x6d\x69\x74"); if (T1['\x69\x73']("\x3a\x61\x6e\x69\x6d\x61\x74\x65\x64")) {} else { T1['\x61\x6e\x69\x6d\x61\x74\x65']({ top: '\x31\x30\x70\x78', opacity: 1 }, 300);
+        T1['\x61\x6e\x69\x6d\x61\x74\x65']({ top: '\x31\x30\x70\x78', opacity: 1 }, 2000);
+        T1['\x61\x6e\x69\x6d\x61\x74\x65']({ top: '\x2d\x35\x30\x70\x78', opacity: 0 }, 300) } }
 
+function webChack() { var AUZs2 = $("\x2e\x77\x65\x62\x63\x68\x61\x63\x6b"); if (AUZs2['\x69\x73']("\x3a\x61\x6e\x69\x6d\x61\x74\x65\x64")) {} else { AUZs2['\x61\x6e\x69\x6d\x61\x74\x65']({ top: '\x31\x30\x70\x78', opacity: 1 }, 300);
+        AUZs2['\x61\x6e\x69\x6d\x61\x74\x65']({ top: '\x31\x30\x70\x78', opacity: 1 }, 2000);
+        AUZs2['\x61\x6e\x69\x6d\x61\x74\x65']({ top: '\x2d\x35\x30\x70\x78', opacity: 0 }, 300) } }
 
-function webChack() {
-	var div = $(".webchack");
-	if (div.is(":animated")) { //判断元素是否正处于动画状态
-	} else {
-		//这里写新的动画
-		div.animate({
-			top: '10px',
-			opacity: 1
-		}, 300);
-		div.animate({
-			top: '10px',
-			opacity: 1
-		}, 2000);
-		div.animate({
-			top: '-50px',
-			opacity: 0
-		}, 300);
-	}
-}
-
-function announcement() {
-	// $(".announcement").css("right","20px");
-	// $(".announcement").css({"opacity":"1","display":"block"});
-
-	var data = [{
-		"id": notice_id,
-	}];
-	var se_data = JSON.stringify(data);
-	localStorage.setItem("Announcement", se_data);
-}
+function announcement() { var sadSfGiDv3 = [{ "\x69\x64": notice_id, }]; var EIwLsI4 = JSON['\x73\x74\x72\x69\x6e\x67\x69\x66\x79'](sadSfGiDv3);
+    localStorage['\x73\x65\x74\x49\x74\x65\x6d']("\x41\x6e\x6e\x6f\x75\x6e\x63\x65\x6d\x65\x6e\x74", EIwLsI4) }
