0577607ec1cb0bac5f76ea81eda9a06019e0604c
ForestTrees <1063319390@qq.com>
2021-03-02 13:19:50 +0800

    update git

--- js/config.js
+++ js/config.js
@@ -2,4 +2,8 @@
 var notice_id = "20210217"; //id编号，通产为发布日期
 var notice_passage = "更新搜索页的“每日一句”，每次刷新时随机显示不同的诗句、短句" //小贴士内容
 
+<<<<<<< HEAD
+var release = "beta 0.2.11" //当前版本
+=======
 var release = "beta 0.2.10" //当前版本
+>>>>>>> 66d4b3f0f49352059584eb46d116c9af604b9255
