93574b09fdd8f530e92363a6c424ec1f9b09d7d3
ForestTrees <1063319390@qq.com>
2021-03-11 11:19:15 +0800

    更改版权声明的呈现样式

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // 小贴士部分
-var notice_id = "20210303"; //id编号，通产为发布日期
-var notice_passage = "1、在“设置”中添加了“建议与反馈”模块<br>2、设置-关于中显示“计划中的功能”" //小贴士内容
+var notice_id = "20210311"; //id编号，通产为发布日期
+var notice_passage = "更改了版权声明的呈现样式，点击底部版权信息获取查看。" //小贴士内容
 
-var release = "beta 0.2.12" //当前版本
+var release = "beta 0.2.13" //当前版本
