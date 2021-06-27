b01edd182a69893465b3be3ce25dcf0fd557ab56
ForestTrees <1063319390@qq.com>
2021-06-27 14:18:10 +0800

    更新天气图标加载动画

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // 小贴士部分
-var notice_id = "20210626"; //id编号，通常为发布日期
-var notice_passage = "调整“建议与反馈”模块为新标签页打开" //小贴士内容
+var notice_id = "20210627"; //id编号，通常为发布日期
+var notice_passage = "1、新标签页打开网址链接</br>2、添加天气图标加载动画" //小贴士内容
 
-var release = "beta 0.2.14" //当前版本
+var release = "beta 0.2.15" //当前版本
