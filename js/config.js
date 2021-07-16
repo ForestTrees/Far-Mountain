089095b96b6dcb57fb7be0c2de45c7dc0f8ac5cc
ForestTrees <1063319390@qq.com>
2021-07-16 23:02:39 +0800

    fix bug

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // 小贴士部分
-var notice_id = "20210627"; //id编号，通常为发布日期
-var notice_passage = "1、新标签页打开网址链接</br>2、添加天气图标加载动画" //小贴士内容
+var notice_id = "20210716"; //id编号，通常为发布日期
+var notice_passage = "1、新标签页打开网址链接</br>2、添加天气图标加载动画</br>3、修改了关于常用链接设置错误的bug" //小贴士内容
 
-var release = "beta 0.2.15" //当前版本
+var release = "beta 0.2.16" //当前版本
